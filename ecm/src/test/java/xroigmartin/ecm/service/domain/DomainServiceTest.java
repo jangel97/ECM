113002ad1d8850fe27ca9437471ed2987fda0be0
Xavier Roig Martin <xroigmartin@gmail.com>
2020-11-14 22:29:47 +0100

    test refactor: Modify DomainServiceTest class

--- ecm/src/test/java/xroigmartin/ecm/service/domain/DomainServiceTest.java
+++ ecm/src/test/java/xroigmartin/ecm/service/domain/DomainServiceTest.java
@@ -3,19 +3,23 @@ package xroigmartin.ecm.service.domain;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import xroigmartin.ecm.model.domain.Domain;
-import xroigmartin.ecm.repository.domain.DomainRepository;
+import xroigmartin.ecm.repository.DomainRepository;
+import xroigmartin.ecm.service.impl.DomainServiceImpl;
 
 class DomainServiceTest {
 	
@@ -23,71 +27,99 @@ class DomainServiceTest {
 	private DomainRepository domainRepositoryMock;
 	
 	@InjectMocks
-	private DomainService domainServiceMock;
+	private DomainServiceImpl domainServiceMock;
 	
 	private Domain domain1, domain2;
 	private List<Domain> domainListMock;
 	
+	@BeforeEach
 	void setUp() {
+		
+		MockitoAnnotations.initMocks(this);
+		
 		domain1 = new Domain(1L, "test1", "test1", true);
-		domain2 = new Domain(2L, "test2", "test2", true);
+		domain2 = new Domain(2L, "test2", "test2", false);
 		domainListMock = new ArrayList<>();
 		domainListMock.add(domain1);
 		domainListMock.add(domain2);
 	}
 
 	@Test
-	void it_should_return_empty_list_test() {
+	void if_not_exists_domains_return_empty_list() {
 		
-		when(domainRepositoryMock.findAll()).thenReturn(new ArrayList<>());
+		List<Domain> domainListMock = new ArrayList<>();
 		
-		List<Domain> domainList = domainServiceMock.findAll();
+		when(domainRepositoryMock.findAll()).thenReturn(domainListMock);
+		
+		List<Domain> domainList = domainServiceMock.findAllDomains();
 		
 		assertTrue(domainList.isEmpty());
 	}
 	
 	@Test
-	void it_should_not_return_empty_list_test() {
+	void if_exists_domains_not_return_empty_list() {
+		
+		List<Domain> domainListMock = new ArrayList<>();
+		
+		domainListMock.add(domain1);
 		
 		when(domainRepositoryMock.findAll()).thenReturn(domainListMock);
 		
-		List<Domain> domainList = domainServiceMock.findAll();
+		List<Domain> domainList = domainServiceMock.findAllDomains();
 		
 		assertFalse(domainList.isEmpty());
 	}
 	
 	@Test
-	void it_should_insert_new_domain_test() {
-		when(domainRepositoryMock.save(domain1)).thenReturn(null);
+	void when_save_domain_call_method_save() {
+
+		when(domainRepositoryMock.save(domain1)).thenReturn(domain1);
 		
-		domainServiceMock.save();
+		domainServiceMock.storeDomain(domain1);
+		
+		verify(domainRepositoryMock, times(1)).save(domain1);
+		
 	}
 	
 	@Test
-	void it_should_not_found_domain_test() {
+	void if_search_domain_with_id_and_not_exists_domain_return_null() {
 		when(domainRepositoryMock.findById(1L)).thenReturn(Optional.empty());
 		
-		Domain domainResult = domainServiceMock.findById(1L);
+		Domain domainResult = domainServiceMock.getDomainById(1L);
 		
 		assertNull(domainResult);
 	}
 	
 	@Test
-	void it_should_found_domain_test() {
+	void if_search_domain_with_id_and_exists_domain_return_domain() {
 
 		when(domainRepositoryMock.findById(1L)).thenReturn(Optional.of(domain1));
 		
-		Domain domainResult = domainServiceMock.findById(1L);
+		Domain domainResult = domainServiceMock.getDomainById(1L);
 		
 		assertTrue(domainResult.equals(domain1));
 	}
 	
 	@Test
-	void it_should_change_state_enable_test() {		
+	void if_damain_is_enable_change_to_disabled() {
+	
 		when(domainRepositoryMock.findById(1L)).thenReturn(Optional.of(domain1));
-		when(domainRepositoryMock.save(domain1)).thenReturn(null);
+		when(domainRepositoryMock.save(domain1)).thenReturn(domain1);
 		
-		domainServiceMock.changeState();
+		domainServiceMock.changeStateDomain(1L);
+		
+		assertFalse(domain1.getEnable());
 	}
 
+	@Test
+	void if_damain_is_disabled_change_to_enabled() {
+	
+		when(domainRepositoryMock.findById(2L)).thenReturn(Optional.of(domain2));
+		when(domainRepositoryMock.save(domain2)).thenReturn(domain2);
+		
+		
+		domainServiceMock.changeStateDomain(2L);
+		
+		assertTrue(domain1.getEnable());
+	}
 }
